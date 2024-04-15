1e79b58760c8934a9e9507510d7fd93bcfa53164
darryl-sangare <darryl.sangare@gmail.com>
2024-04-15 02:53:03 +0200

    Setup 1

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Routes, Route} from "react-router-dom"
+
+function App() {
+
+
+  return (
+    <div>
+      <Routes>
+
+        
+
+
+
+
+      </Routes>
+
+
+    </div>
+
+  )
+}
+
+export default App
